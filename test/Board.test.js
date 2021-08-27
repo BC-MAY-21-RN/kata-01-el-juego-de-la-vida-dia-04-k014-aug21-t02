76bff02b405f7eaded967ce41d982dbd3397ca8c
Shadic78 <carlosjosue7889@gmail.com>
2021-08-27 11:33:25 -0500

    Add Jest test for Board class
    Co-authored-by: Vladimir-Maidana <maidana.vladi@gmail.com>

---
+++ test/Board.test.js
@@ -0,0 +1,9 @@
+import { Board } from '../src/Board.js';
+
+test("Generate matrix test", () => {
+    const testMatrixRows = 2;
+    const testMatrixCols = 2;
+    const testMatrix = [[".", "."], [".", "."]];
+    const board = new Board(testMatrix);
+    expect(board.generateNewMatrix(testMatrixRows, testMatrixCols)).toEqual(testMatrix);
+});
