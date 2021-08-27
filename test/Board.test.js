015ba30236a7ed0102dc65d94d0899332cc38a26
Shadic78 <carlosjosue7889@gmail.com>
2021-08-27 12:05:29 -0500

    Add test for Board class functions
    Co-authored-by: Vladimir-Maidana <maidana.vladi@gmail.com>

--- test/Board.test.js
+++ test/Board.test.js
@@ -7,3 +7,24 @@ test("Generate matrix test", () => {
     const board = new Board(testMatrix);
     expect(board.generateNewMatrix(testMatrixRows, testMatrixCols)).toEqual(testMatrix);
 });
+
+test("Get neighbors test", () => {
+    const testMatrix = [[".", ".", "*"], [".", "*", "."]];
+    const board = new Board(testMatrix);
+    const expectedNeighbors = ["*", ".", ".", undefined];
+    expect(board.getNeighbors(testMatrix, 0, 2)).toEqual(expectedNeighbors);
+});
+
+test("Count alive neighbors test", () => {
+    const testMatrix = [[".", ".", "*"], [".", "*", "."]];
+    const board = new Board(testMatrix);
+    const neighbors = board.getNeighbors(testMatrix, 0, 2);
+    expect(board.countLiveNeighbors(neighbors)).toBe(1);
+});
+
+test("Get matrix test", () => {
+    const testMatrix = [[".", ".", "*"], [".", "*", "."]];
+    const expectedMatrix = [[".", ".", "."], [".", ".", "."]];
+    const board = new Board(testMatrix);
+    expect(board.getMatrix()).toEqual(expectedMatrix);    
+});
